a53ca648a2bd40e4b48ebcbeee040cc1b69e486e
Engineer-cat <estromberg224@gmail.com>
2025-11-07 13:54:12 +0100

    Flex media grej fix grej

--- media-display-page.php
+++ media-display-page.php
@@ -64,7 +64,7 @@ if (!isset($_SESSION['user_id'])) {
 
     </menu>
     <main>
-        <div id="media-catalog"class="media-grid">
+        <div id="media-catalog">
             <div>
                 <p>Test: &#128191;</p>
                 <ul id="media-container">
